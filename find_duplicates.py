48383c57c665a365bef7002d3adb993f25f77e46
Dmitry Grachev <dimasg@gmail.com>
2023-10-22 14:19:38 +0200

    added --ignore-dir option

--- find_duplicates.py
+++ find_duplicates.py
@@ -10,8 +10,13 @@ def find_duplicates(cmd_args):
     """ find duplicates """
     files_info = {}
 
-    for root, dirs, files in os.walk(cmd_args.dir):
+    for root, dirs, files in os.walk(cmd_args.dir, topdown=True):
         logging.debug('Walking ... %s, %s, %s', root, dirs, files)
+        for next_dir in cmd_args.ignore_dir:
+            if next_dir in dirs:
+                logging.debug('%s will be skipped', next_dir)
+                dirs.remove(next_dir)
+
         for file_name in files:
             file_path = os.path.join(root, file_name)
             stats = os.stat(file_path)
@@ -21,6 +26,7 @@ def find_duplicates(cmd_args):
                 files_info[file_name].append(file_info)
             else:
                 files_info[file_name] = [file_info]
+
     logging.debug('Files:\n%s', files_info)
 
     print(f'Found files: {len(files_info.keys())}')
@@ -41,6 +47,11 @@ def main():
     parser.add_argument(
         'dir', action='store',
         help='directory to find'
+    )
+    parser.add_argument(
+        '--ignore-dir', action='store',
+        nargs='*', default=[],
+        help='ignore one or more dirs'
     )
     parser.add_argument(
         '--debug', action='store_const',
