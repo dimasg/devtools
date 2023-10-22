d90ff4c9402b3ede975b0f12c237a04dfa0d2cf2
Dmitry Grachev <dimasg@gmail.com>
2023-10-22 23:55:46 +0200

    find duplicates with same name and size, first version

--- find_duplicates.py
+++ find_duplicates.py
@@ -21,11 +21,21 @@ def find_duplicates(cmd_args):
             file_path = os.path.join(root, file_name)
             stats = os.stat(file_path)
             logging.debug('File %s stats %s', file_path, stats)
-            file_info = {'path': root, 'size': stats.st_size}
-            if file_name in files_info:
-                files_info[file_name].append(file_info)
+            size = stats.st_size
+            file_info = {'path': root, 'size': size}
+            if cmd_args.ignore_size:
+                if file_name in files_info:
+                    files_info[file_name].append(file_info)
+                else:
+                    files_info[file_name] = [file_info]
             else:
-                files_info[file_name] = [file_info]
+                if file_name in files_info:
+                    if size in files_info[file_name]:
+                        files_info[file_name][size].append(file_info)
+                    else:
+                        files_info[file_name][size] = [file_info]
+                else:
+                    files_info[file_name] = {size: [file_info]}
 
     logging.debug('Files:\n%s', files_info)
 
@@ -33,12 +43,19 @@ def find_duplicates(cmd_args):
     print('Duplicates found:')
 
     for file_name, infos in files_info.items():
-        if len(infos) < 2:
-            continue
-        print(f'File {file_name} found in:'.format(file_name))
-        for info in infos:
-            print(f"\tsize: {info['size']}, path: {info['path']}")
-
+        if cmd_args.ignore_size:
+            if len(infos) < 2:
+                continue
+            print(f'File {file_name} found in:'.format(file_name))
+            for info in infos:
+                print(f"\tsize: {info['size']}, path: {info['path']}")
+        else:
+            for file_size, size_infos in infos.items():
+                if len(size_infos) < 2:
+                    continue
+                print(f'File {file_name} with size {file_size} found in:')
+                for info in size_infos:
+                    print(f"\tpath: {info['path']}")
 
 def main():
     """ main subroutine """
@@ -52,6 +69,11 @@ def main():
         '--ignore-dir', action='store',
         nargs='*', default=[],
         help='ignore one or more dirs'
+    )
+    parser.add_argument(
+        '--ignore-size', action='store_const',
+        const=True, default=False,
+        help='ignore files size'
     )
     parser.add_argument(
         '--debug', action='store_const',
