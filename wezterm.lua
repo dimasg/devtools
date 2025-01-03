ea72cd7cbab2571f704da3108eafdc4300e39112
Dmitry Grachev <dimasg@gmail.com>
2025-01-03 19:00:33 +0100

    Initial config for wezterm and ghostty

---
+++ wezterm.lua
@@ -0,0 +1,16 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = 'Hipster Green'
+
+-- https://github.com/be5invis/Iosevka/
+config.font = wezterm.font('Iosevka Term Curly', {weight = 'Medium'}) 
+config.font_size = 14
+
+-- and finally, return the configuration to wezterm
+return config
