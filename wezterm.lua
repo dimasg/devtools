20cbb6518046458a9b383bf9746f3ec589cf4076
Dmitry Grachev <dimasg@gmail.com>
2025-01-12 23:36:32 +0100

    wezterm: tail space removed

--- wezterm.lua
+++ wezterm.lua
@@ -10,7 +10,7 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Hipster Green'
 
 -- https://github.com/be5invis/Iosevka/
-config.font = wezterm.font('Iosevka Term Curly', {weight = 'Medium'}) 
+config.font = wezterm.font('Iosevka Term Curly', {weight = 'Medium'})
 config.font_size = 14
 
 config.initial_rows = 40
