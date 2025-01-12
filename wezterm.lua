8a72ef03240eb78d88955ef7270d351cc4606aae
Dmitry Grachev <dimasg@gmail.com>
2025-01-12 23:30:58 +0100

    wezterm: select active pane select shortcut

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -15,5 +16,17 @@ config.font_size = 14
 config.initial_rows = 40
 config.initial_cols = 132
 
+config.keys = {
+    -- show the pane selection mode, but have it swap the active and selected panes
+    {
+        key = '0',
+        mods = 'CTRL',
+        action = act.PaneSelect {
+            mode = 'SwapWithActive',
+        },
+    },
+}
+
 -- and finally, return the configuration to wezterm
 return config
+-- vim: ts=4 sw=4
